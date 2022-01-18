61b0311b74225a09464f98fe74012b0a2f60c0c0
wmerians <w.merians@gmail.com>
2022-01-17 19:35:18 -0800

    Updated Default data in MockDataStore.cs

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -14,12 +14,11 @@ namespace Mine.Services
         {
             items = new List<ItemModel>()
             {
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Banna Peal", Description="Causes cart behind to slip.", Value=5 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Red Shell", Description="Auto targets the cart infront.", Value=1 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Triple Red Shell", Description="3 Red Shells.", Value=3 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Green Shell", Description="Shoots forward Unguided.", Value=6 },
+                new ItemModel { Id = Guid.NewGuid().ToString(), Text = "Lightning Bolt", Description="Causes others to shrink.", Value=9 },
             };
         }
 
