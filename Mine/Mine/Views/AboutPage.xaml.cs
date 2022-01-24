93bc23e70a2b655be730e45324cb8249f04d933a
wmerians <w.merians@gmail.com>
2022-01-23 17:03:13 -0800

    Added Today’s DateTime to AboutPage, changed AboutPage.xaml, AboutPage.xaml.cs

--- Mine/Mine/Views/AboutPage.xaml.cs
+++ Mine/Mine/Views/AboutPage.xaml.cs
@@ -13,6 +13,7 @@ namespace Mine.Views
         public AboutPage()
         {
             InitializeComponent();
+            CurrentDateTimeLabel.Text = System.DateTime.Now.ToString("MM/dd/yy hh:mm:ss");
         }
     }
 }
