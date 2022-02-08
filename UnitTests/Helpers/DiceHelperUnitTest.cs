1e5f1e73408e1a47804d3c847498d68b5527fe79
wmerians <w.merians@gmail.com>
2022-02-07 16:11:22 -0800

    added RollDice_InValid_Roll_0_Dice_10_Should_Return_Zero method to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -50,6 +50,20 @@ namespace UnitTests.Helpers
         }
 
         [Test]
+        public void RollDice_InValid_Roll_0_Dice_10_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(0, 10);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
         public void RollDice_Invalid_Roll_Forced_1_Should_Return_1()
         {
             // Arrange
