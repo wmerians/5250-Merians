aad32cb5b25fcd281913f2f4dfca2f66b5bed5f4
wmerians <w.merians@gmail.com>
2022-02-07 16:13:13 -0800

    added RollDice_Valid_Roll_1_Dice_10_Fixed_5_Should_Return_5 method to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -93,5 +93,22 @@ namespace UnitTests.Helpers
             // Assert
             Assert.AreEqual(1, result);
         }
+
+        [Test]
+        public void RollDice_Valid_Roll_1_Dice_10_Fixed_5_Should_Return_5()
+        {
+            // Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 5;
+
+            // Act
+            var result = DiceHelper.RollDice(1, 10);
+
+            // Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
     }
 }
